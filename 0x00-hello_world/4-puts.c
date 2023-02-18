24b3c2a079b295b8a3a8532d93be081916c8c028
Jidotcom <adenikesalaudeen5@gmail.com>
2023-02-18 08:25:35 -0800

    c programming basics

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+# coding using the pits function
+/**
+ * main - entry point for compilation
+ *
+ * Return: Always 0 for correct code
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+# This explains the use of printf in coding
+/**
+ * main - entry point for coding
+ *
+ * Return: Always 0 to be correct
+ */
+int main(void)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
