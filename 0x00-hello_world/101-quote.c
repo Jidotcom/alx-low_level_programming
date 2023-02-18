9d006aeecd2fd1b522abcb945f0f86a81010aaac
Jidotcom <adenikesalaudeen5@gmail.com>
2023-02-18 09:43:26 -0800

    using write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of c
+ *
+ * Return - return 1 (success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\"
+- Dora Korpar, 2015-10-19\n", 61);
+	Return (1);
+}
